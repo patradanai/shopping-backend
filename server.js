3b5b68ae8fe55bd8a687d18d54fa66b8c9322259
Patradanai <patradanai@Patradanais-MacBook-Pro.local>
2021-04-26 23:47:37 +0700

    Signin and Signup Complete

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./src/models");
 const cors = require("cors");
-
+const isForceDb = false;
 const AuthRoutes = require("./src/routes/Auth.routes");
 
 const app = express();
@@ -17,10 +17,19 @@ app.use("/auth", AuthRoutes);
 
 // Run Server
 db.sequelize
-  .sync()
+  .sync({ force: isForceDb })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server RUNNING ON PORT : ${PORT}`);
     });
+  })
+  .then(() => {
+    // Init Database
+    // db.Role.bulkCreate([
+    //   { role: "Administrator" },
+    //   { role: "Staff" },
+    //   { role: "Customer" },
+    //   { role: "Moderator" },
+    // ]);
   })
   .catch((err) => console.log(err));
--- src/controllers/Auth.Controller.js
+++ src/controllers/Auth.Controller.js
@@ -3,9 +3,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = db.User;
 const Role = db.Role;
-const expired = 60 * 60;
 
-exports.signIn = async (res, req) => {
+exports.signIn = async (req, res) => {
   const { username, password } = req.body;
 
   // Validate User and Password
@@ -13,28 +12,65 @@ exports.signIn = async (res, req) => {
     return res.status(400).json({ message: "Invalid User or Password" });
   }
 
-  // Check User in db
-  const user = await User.findOne({ where: { username: username } });
-  if (!user) {
-    return res.status(404).json({ message: "User not exsting" });
+  try {
+    // Check User in db
+    const user = await User.findOne({ where: { username: username } });
+    if (!user) {
+      return res.status(404).json({ Error: "User not exsting" });
+    }
+
+    // Compare Password
+    const comparePassword = await bcrypt.compareSync(
+      password.toString(),
+      user.password
+    );
+    if (!comparePassword) {
+      return res.status(401).json({ Error: "User or Password invalid" });
+    }
+
+    // Token
+    const token = jwt.sign(
+      { userId: user.id, name: user.fname + user.lname },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({ token: token });
+  } catch (err) {
+    return res.status(500).json({ Error: err.message });
   }
-
-  // Compare Password
-  const comparePassword = await bcrypt.compareSync(password, user.password);
-  if (!comparePassword) {
-    return res.status(401).json({ message: "User or Password invalid" });
-  }
-
-  // Token
-  const token = jwt.sign(
-    { userId: user.id, name: user.fname + user.lname },
-    process.env.SECRET_KEY,
-    { expiresIn: "1h" }
-  );
-
-  return res.status(200).json({ token: token });
 };
 
-exports.signUp = (res, req) => {
-  const {} = req.body;
+exports.signUp = async (req, res) => {
+  const { email, fname, lname, password } = req.body;
+
+  try {
+    // Check Existing in Database
+    const user = await User.findOne({ where: { username: email } });
+    if (user) {
+      return res.status(422).json({ Error: "User existing" });
+    }
+
+    // Hashing and Create in DB
+    const hashedPw = await bcrypt.hashSync(password.toString(), 10);
+
+    // Create in Db
+    const userSaved = await User.create({
+      username: email,
+      password: hashedPw,
+      email: email,
+      fname: fname,
+      lname: lname,
+    });
+
+    // Set Default Role
+    const role = await Role.findOne({ where: { role: "Administrator" } });
+
+    // Save Role
+    await userSaved.setRoles(role);
+
+    return res.status(200).json({ message: "Register Complete" });
+  } catch (err) {
+    return res.status(500).json({ Error: err.message });
+  }
 };
