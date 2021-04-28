444b2a3c55e461e06e0eace05a83b439ff71640f
Patradanai <patradanai@Patradanais-MacBook-Pro.local>
2021-04-28 14:31:39 +0700

    get CartController

--- src/controllers/Cart.controller.js
+++ src/controllers/Cart.controller.js
@@ -1,9 +1,17 @@
 const db = require("../models");
+const User = db.User;
 
-exports.getCart = (req, res) => {
+exports.getCart = async (req, res) => {
   const userId = req.userId;
 
   try {
+    // Check User
+    const userInstance = await User.findByPk(userId);
+
+    // Get Cart
+    const cartInstance = await userInstance.getCart();
+
+    return res.status(200).json({ data: cartInstance });
   } catch (err) {
     return res.status(500).json({ Error: err.message });
   }
