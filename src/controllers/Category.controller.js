e9e0b06b750904bb941e8aea10860fc1b2f77197
Patradanai <bloody_jojo@hotmail.com>
2021-05-07 16:02:45 +0700

    add category by Id

--- src/controllers/Category.controller.js
+++ src/controllers/Category.controller.js
@@ -4,10 +4,25 @@ const Category = db.Category;
 exports.readCategory = async (req, res) => {
   try {
     const productInstance = await Category.findAll({
-      attributes: ["id", "name"],
+      attributes: ["id", "name", "imageSrc"],
     });
 
     return res.status(200).json({ data: productInstance });
+  } catch (err) {
+    return res.status(500).json({ Error: err.message });
+  }
+};
+
+exports.categoryById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    //get Category instance
+    const categoryInstance = await Category.findByPk(id);
+
+    // getChild Product
+    const products = await categoryInstance.getProducts();
+
+    return res.status(200).json(products);
   } catch (err) {
     return res.status(500).json({ Error: err.message });
   }
--- src/routes/Category.routes.js
+++ src/routes/Category.routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const Router = express.Router();
-const { readCategory } = require("../controllers/Category.controller");
+const {
+  readCategory,
+  categoryById,
+} = require("../controllers/Category.controller");
 
 // GET /:shopId/categories
 Router.get("/categories", readCategory);
+
+// GET /categories/:id
+Router.get("/categories/:id", categoryById);
 
 module.exports = Router;
